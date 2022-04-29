0084ac35e48cb60024f4f0a43bb5fca24835eb77
Folderas <82723806+marcinwolder@users.noreply.github.com>
2022-04-29 20:54:47 +0200

    1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Hi there!');
+async function fetchData(params) {
+  const result = await axios.get('http://www.omdbapi.com/', {
+    params: { ...params, apikey: 'cff9ad86' },
+  });
+  return result;
+}
+
+// const firstFilm = fetchData({ s: 'spiderman' }).then((x) => x.data.Search);
+// firstFilm.then((data) => {
+//   console.log(data);
+// });
+
+let result;
+(async function () {
+  result = await fetchData({ s: 'avengers' }).then((data) => data);
+})();
+
+const filmName = document.querySelector('#filmName');
+
+let timeoutId;
+filmName.addEventListener('input', (event) => {
+  if (timeoutId) clearTimeout(timeoutId);
+  timeoutId = setTimeout(() => {
+    console.log(event.target.value);
+  }, 500);
+});
